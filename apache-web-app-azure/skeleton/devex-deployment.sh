6f4fcc4ab495f6869bd495855228db6abb485839
Parthbhrg <113418666+Parthbhrg@users.noreply.github.com>
2024-02-09 09:21:20 +0530

    Update devex-deployment.sh

--- apache-web-app-azure/skeleton/devex-deployment.sh
+++ apache-web-app-azure/skeleton/devex-deployment.sh
@@ -1,5 +1,7 @@
 az login
 
 terraform init
+curl -X POST -H 'Content-Type: application/json' -d '{"trackId": "12345", "status": "InProgress"}'  http://20.193.131.46:4001/api/v2/updatestatus
 terraform plan
 terraform apply -auto-approve
+curl -X POST -H 'Content-Type: application/json' -d '{"trackId": "12345", "status": "Success"}'  http://20.193.131.46:4001/api/v2/updatestatus
