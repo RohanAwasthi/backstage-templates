1994670ae0c751124b78dc819690b615a20068c9
Parthbhrg <113418666+Parthbhrg@users.noreply.github.com>
2024-03-01 18:10:09 +0530

    Update providers.tf

--- GCP-Cluster/skeleton/project/providers.tf
+++ GCP-Cluster/skeleton/project/providers.tf
@@ -9,7 +9,7 @@ terraform {
 
 // provider "google" {
 //  project     = "first-407310"
-//  # credentials = file("C:/Users/WY182AQ/Downloads/first-407310-a78185d4f709.json")
+//   credentials = file("C:/Users/WY182AQ/Downloads/first-407310-a78185d4f709.json")
 // }
 
 provider "google" {
