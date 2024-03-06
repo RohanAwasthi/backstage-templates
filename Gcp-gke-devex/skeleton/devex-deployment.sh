38a08881effb0f7074219350d56d24488c3ab363
Devgit10 <143741033+Devgit10@users.noreply.github.com>
2024-03-06 12:54:59 +0530

    Update devex-deployment.sh

--- Gcp-gke-devex/skeleton/devex-deployment.sh
+++ Gcp-gke-devex/skeleton/devex-deployment.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 export PATH=$PATH:/root/gcp/google-cloud-sdk/bin
 gcloud auth activate-service-account --key-file=/root/credentials.json
-cd /root/gcpgketemplate/GCP-Cluster/skeleton/project
+cd project
 terraform init
 terraform plan
 terraform apply -auto-approve
