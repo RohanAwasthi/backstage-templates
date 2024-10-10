034e76e50808836cb88117828b6b15a01bd64b3e
Parthbhrg <113418666+Parthbhrg@users.noreply.github.com>
2024-10-10 17:12:48 +0530

    Update provider.tf

--- apache-azure-app-v3/infra/provider.tf
+++ apache-azure-app-v3/infra/provider.tf
@@ -11,7 +11,11 @@ terraform {               //for log in ssh azureuser@publicip
 provider "azurerm" {
   skip_provider_registration = true
   
-  features {}
+  features {
+   resource_group {
+    prevent_deletion_if_contains_resources = false
+                  }
+           } 
   //subscription_id = ""
   
 }
